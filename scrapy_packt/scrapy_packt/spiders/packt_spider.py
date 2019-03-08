afeb6ea2273b7417fab8f87f011f83da58e24c34
Andrew Key <joeandrewkey@gmail.com>
2019-03-07 22:37:54 -0800

    Adding a new .pycharmrc file

--- scrapy_packt/scrapy_packt/spiders/packt_spider.py
+++ scrapy_packt/scrapy_packt/spiders/packt_spider.py
@@ -9,25 +9,22 @@ class PaktSpider(scrapy.Spider):
     ]
 
     def parse_second_page(self, response):
-        bonus = response.meta.get("bonus")
-        bonus["author"] = response.css('div.book-info-bottom-author-title').xpath(".//h3/text()").extract_first().strip()
-        bonus["description"] = response.xpath("//meta[@name='description']/@content")[0].extract().strip()
-        bonus["datePublished"] = response.xpath("//time[@itemprop='datePublished']/@datetime")[0].extract().strip()
-        bonus["bookDescription"] = response.xpath("//div[@class='book-info-bottom-indetail-text']//p//node()").extract()
-        yield bonus
+        packt_record = response.meta.get("packt_record")
+        packt_record["author"] = response.css('div.book-info-bottom-author-title').xpath(".//h3/text()").extract_first().strip()
+        packt_record["description"] = response.xpath("//meta[@name='description']/@content")[0].extract().strip()
+        packt_record["datePublished"] = response.xpath("//time[@itemprop='datePublished']/@datetime")[0].extract().strip()
+        packt_record["bookDescription"] = response.xpath("//div[@class='book-info-bottom-indetail-text']//p//node()").extract()
+        yield packt_record
 
     def parse(self, response):
         for book_block in response.css('div.book-block'):
             overlay = book_block.css('div.book-block-overlay').xpath(".//button").attrib
-            next_page = "https://www.packtpub.com" + overlay['action']
-            bonus = {
+            book_page = "https://www.packtpub.com" + overlay['action']
+            packt_record = {
                 'title': book_block.css('div.book-block-title::text').getall()[-1].strip(),
                 'price_full': book_block.css("div.book-block-price-full::text").getall()[-1].strip(),
                 'price_discounted': book_block.css("div.book-block-price-discounted::text").getall()[-1].strip(),
-                'release_date': book_block.css('div.book-block-overlay-release-date::text').getall()[-1].strip(),
                 'length': book_block.css('div.book-block-overlay-product-length::text').getall()[-1],
-                'nid': overlay['nid'],
-                'action': overlay['action'],
-                'class': overlay['class'],
+                'book_page': book_page,
             }
-            yield scrapy.Request(url=next_page, callback=self.parse_second_page, meta={"bonus": bonus})
+            yield scrapy.Request(url=book_page, callback=self.parse_second_page, meta={"packt_record": packt_record})
